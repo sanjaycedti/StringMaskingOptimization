a5da406509ce8d14b17561bf714ec7b9bbf7061f
Sanjay Sahani <sanjay.sahani@appinventiv.com>
2023-03-18 19:23:44 +0530

    first commit

---
+++ Masking/Benchmark.cs
@@ -0,0 +1,91 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+
+namespace Masking;
+
+[MemoryDiagnoser]
+public class MaskingBenchmark
+{
+	private const string ClearValue = "Secret Password";
+
+	[Benchmark]
+	public string MaskStringConcate()
+	{
+		var value = ClearValue.Substring(0, 3);
+		var length = ClearValue.Length - value.Length;
+
+		for(var i = 0; i < length; i++)
+		{
+			value += "*";
+		}
+
+		return value;
+	}
+
+    [Benchmark]
+    public string MaskStringBuilder()
+    {
+        var text = ClearValue.Substring(0, 3);
+        var sb = new StringBuilder(text);
+        var length = ClearValue.Length - text.Length;
+
+        for (var i = 0; i < length; i++)
+        {
+			sb.Append('*');
+        }
+
+        return sb.ToString();
+    }
+
+    [Benchmark]
+    public string MaskNewString()
+    {
+        var text = ClearValue.Substring(0, 3);
+        var length = ClearValue.Length - text.Length;
+
+        var str = new string('*', length);
+
+        return text + str;
+    }
+
+    [Benchmark]
+    public string MaskCharArray()
+    {
+        var chars = ClearValue.ToCharArray();
+
+        for (var i = 3; i < ClearValue.Length; i++)
+        {
+            chars[i] = '*';
+        }
+
+        return new string(chars);
+    }
+
+    [Benchmark]
+    public string MaskStringCreate()
+    {
+        return string.Create(ClearValue.Length, ClearValue, (span, value) =>
+        {
+            value.AsSpan().CopyTo(span);
+
+            span[3..].Fill('*');
+        });
+    }
+
+    [Benchmark]
+    public string MaskSpan()
+    {
+        Span<char> span = stackalloc char[ClearValue.Length];
+
+        for(var i = 0; i < 3; i++)
+        {
+            span[i] = ClearValue[i];
+        }
+
+        span[3..].Fill('*');
+
+        return span.ToString();
+    }
+}
+
---
+++ Masking/Program.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Running;
+using Masking;
+
+BenchmarkRunner.Run<MaskingBenchmark>();
+
+return;
+
+var benchmark = new MaskingBenchmark();
+
+var str1 = benchmark.MaskStringConcate();
+
+Console.WriteLine(str1);
+
+var str2 = benchmark.MaskStringBuilder();
+
+Console.WriteLine(str2);
+
+var str3 = benchmark.MaskNewString();
+
+Console.WriteLine(str3);
+
+var str4 = benchmark.MaskCharArray();
+
+Console.WriteLine(str4);
+
+var str5 = benchmark.MaskStringCreate();
+
+Console.WriteLine(str5);
+
+var str6 = benchmark.MaskSpan();
+
+Console.WriteLine(str6);
